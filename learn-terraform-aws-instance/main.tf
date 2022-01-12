7a8876857a6629f1a5c3dc0236231b28a6f96c3d
Hamid Gholami <hidgholami@gmail.com>
2022-01-12 18:11:20 +0400

    Added new main.tf for creating an AWS EC2 instance
    Signed-off-by: Hamid Gholami <hidgholami@gmail.com>

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  #   ami           = "ami-830c94e3"
+  ami           = "ami-08e4e35cccc6189f4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
