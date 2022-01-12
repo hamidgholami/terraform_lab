236924a9129467ebea8f604f82c2929c5c0c6f08
Hamid Gholami <hidgholami@gmail.com>
2022-01-12 18:45:11 +0400

    Define input variables
    Signed-off-by: Hamid Gholami <hidgholami@gmail.com>

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -20,6 +20,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    # Name = "ExampleAppServerInstance"
+    Name = var.instance_name # Also you can use this command: terraform apply -var "instance_name=YetAnotherName"
   }
 }
